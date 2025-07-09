cf70e76f20fd7b159a807ff190e4f2c49424a6a1
hoangphivo97 <phivo1397@gmail.com>
2025-07-09 16:25:27 +0700

    some UIUx for sidebar

--- mfe-shell-angular/src/app/shared/components/sidebar/sidebar.component.ts
+++ mfe-shell-angular/src/app/shared/components/sidebar/sidebar.component.ts
@@ -4,6 +4,9 @@ import { ExpenseListComponent } from '../../../features/expenses/expense-list/ex
 import { AuthService } from '../../../services/RouteGuard/auth.service';
 import { CommonModule } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
+import { DialogActionEnum, DialogData } from '../../../interface/modal.interface';
+import { MatDialog } from '@angular/material/dialog';
+import { SettingsModalComponent } from '../../../modal/settings-modal/settings-modal.component';
 
 @Component({
   selector: 'app-sidebar',
@@ -13,15 +16,30 @@ import { MatIcon } from '@angular/material/icon';
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent implements OnInit {
+  readonly dialog = inject(MatDialog)
   authService = inject(AuthService);
   user$ = this.authService.user$;
   collapsed = false;
+  dialogActionEnum = DialogActionEnum
 
   ngOnInit(): void {
 
   }
 
+  logout() {
+    this.authService.logout();
+  }
 
+  openSettings() {
+    const dialogRef = this.dialog.open(SettingsModalComponent, {
+      height: '270px',
+      width: '450px',
+      data: { title: "Settings", action: this.dialogActionEnum.Settings, isSuccess: false } as DialogData,
+      disableClose: true
+    })
+
+    // this.getListAfterSuccessCallApi(dialogRef)
+  }
 
   toggleSidebar() {
     this.collapsed = !this.collapsed;
