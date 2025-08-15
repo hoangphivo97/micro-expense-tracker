7058d50a930830765e12e81e1959a432ef0cd33f
hoangphivo97 <phivo1397@gmail.com>
2025-08-15 15:23:21 +0700

    install ng-apexchart and demo

--- mfe-shell-angular/src/app/features/expenses/report/report.component.ts
+++ mfe-shell-angular/src/app/features/expenses/report/report.component.ts
@@ -1,16 +1,54 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { HeaderComponent } from "../../../shared/components/header/header.component";
 import { FilterComponent } from "../../../shared/components/filter/filter.component";
 import { Router } from '@angular/router';
 import { MatIcon } from '@angular/material/icon';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexTitleSubtitle,
+  NgApexchartsModule
+} from "ng-apexcharts";
 
 @Component({
   selector: 'app-report',
   standalone: true,
-  imports: [HeaderComponent, FilterComponent, MatIcon],
+  imports: [HeaderComponent, FilterComponent, MatIcon, NgApexchartsModule],
   templateUrl: './report.component.html',
-  styleUrl: './report.component.scss'
+  styleUrl: './report.component.scss',
 })
 export class ReportComponent {
+  @ViewChild("chart") chart: ChartComponent | undefined;
+  public chartOptions: Partial<ChartOptions>;
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "My-series",
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "bar"
+      },
+      title: {
+        text: "My First Angular Chart"
+      },
+      xaxis: {
+        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"]
+      }
+    };
+  }
 
 }
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
+};
