b0642477628b094a752c07f586d0b1d980fcf940
hoangphivo97 <phivo1397@gmail.com>
2025-09-11 14:43:04 +0700

    adding data-testid for cypress automation test

---
+++ mfe-shell-angular/cypress/e2e/expense.crud.cy.ts
@@ -0,0 +1,40 @@
+describe('Expense', () => {
+  before(() => {
+    cy.session('qa-user-001', () => {
+      cy.task<string>('mintCustomToken', 'qa-user-001').then((token) => {
+        cy.visit('/'); //Root open
+
+        //Sign In with custom token in browser
+        cy.window().then(async () => {
+          const { getAuth, setPersistence, browserLocalPersistence, signInWithCustomToken } = await import('firebase/auth')
+          const { getApp, initializeApp, getApps } = await import('firebase/app')
+          const { firebaseConfig } = await import("../../src/app/environment/environment")
+
+          const app = getApps().length ? getApp() : initializeApp({
+            ...firebaseConfig
+          })
+          const auth = getAuth(app);
+          await setPersistence(auth, browserLocalPersistence)
+          await signInWithCustomToken(auth, token);
+        });
+      });
+    });
+  });
+  beforeEach(() => {
+    cy.visit('/expenses');
+    cy.contains('Expenses').should('be.visible');
+  });
+
+  it('open create modal', () => {
+    cy.get('[data-testid="open-create-modal-btn"]').click()
+    cy.get('mat-dialog-container').should('be.visible')
+  });
+
+  it('create expense', () => {
+    cy.get('[data-testid="open-create-modal-btn"]').click()
+    cy.get('mat-dialog-container').should('be.visible')
+
+    cy.get('[data-testid="expense-date-toggle"]').click()
+    cy.get('.mat-datepicker-content').should('be.visible')
+  })
+})
