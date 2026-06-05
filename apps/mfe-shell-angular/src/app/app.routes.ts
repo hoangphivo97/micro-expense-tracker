67eb768867f8cce3aa1a04c7609985438ca0a1e0
hoangphivo97 <phivo1397@gmail.com>
2026-06-05 21:44:15 +0700

    delete code make boilerplate

--- apps/mfe-shell-angular/src/app/app.routes.ts
+++ apps/mfe-shell-angular/src/app/app.routes.ts
@@ -38,10 +38,4 @@ export const routes: Routes = [
   },
 ];
 
-@NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
-  ],
-  exports: [RouterModule],
-})
 export class AppRoutingModule {}
